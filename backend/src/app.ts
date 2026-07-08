6653b4ea0733e53830cf4105fec00c21928dfd86
miletalvova <mileta279@gmail.com>
2026-07-08 19:25:42 +0200

    cors domain

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'https://service-tracking-platform.vercel.app',
     'https://service-tracking-platform-git-main-miletas-projects-977d2388.vercel.app',
     'https://service-tracking-platform-kbr1pq0uu-miletas-projects-977d2388.vercel.app'
   ],
